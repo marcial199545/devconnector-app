862970e147e1aad6ef816489ba1b1e7e77d18ee7
Marcial <marcial.jimenez.caballero@gmail.com>
2019-06-24 12:11:58 -0700

    created test routes for profile, auth, posts, users and also created the model for User

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get("/", (req, res) => {
     res.send("hello from express");
 });
 
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(PORT, () => {
     console.log(`listening to port: ${PORT}`);
 });
