3513833bc5bcb08154017794e9eca05244721f7f
Marcial <marcial.jimenez.caballero@gmail.com>
2019-06-28 09:09:42 -0700

    created the protectedroute component to access the dashboard page 🐲

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         name: "",
         email: "",
@@ -13,11 +17,14 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log("Password do not match");
+            setAlert("Passwords do not match", "danger", 2000);
         } else {
-            console.log("success");
+            register({ name, email, password });
         }
     };
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />;
+    }
     return (
         <Fragment>
             <section className="container">
@@ -80,5 +87,15 @@ const Register = () => {
         </Fragment>
     );
 };
-
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+export default connect(
+    mapStateToProps,
+    { setAlert, register }
+)(Register);
