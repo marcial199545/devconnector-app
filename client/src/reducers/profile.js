7e595db3b17aa86e5faddc593d4d76b6a0dd8dfe
Marcial <marcial.jimenez.caballero@gmail.com>
2019-06-29 16:10:06 -0700

    created the component to update the profile and add experiences and ecducation, also created the action to edit the  profile experience and education

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
 
 const initialState = {
     profile: null,
@@ -18,6 +18,12 @@ export default function(state = initialState, action) {
                 profile: payload,
                 loading: false
             };
+        case UPDATE_PROFILE:
+            return {
+                ...state,
+                profile: payload,
+                loading: false
+            };
         case PROFILE_ERROR:
             return {
                 ...state,
@@ -31,7 +37,6 @@ export default function(state = initialState, action) {
                 repos: [],
                 loading: false
             };
-
         default:
             return state;
     }
